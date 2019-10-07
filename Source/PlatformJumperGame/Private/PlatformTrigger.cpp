15a55040fc687db39fe922833b204449678c9bf9
Yernar Aldabergenov <yernar.aa@gmail.com>
2019-10-07 12:31:07 +0600

    Platform triggers have been added; | Begin & End Overlap events implemented.

---
+++ Source/PlatformJumperGame/Private/PlatformTrigger.cpp
@@ -0,0 +1,46 @@
+// Copyright (C) 2019 Yernar Aldabergenov. All Rights Reserved.
+
+
+#include "PlatformTrigger.h"
+
+#include "Components/BoxComponent.h"
+
+// Sets default values
+APlatformTrigger::APlatformTrigger()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	TriggerVolume = CreateDefaultSubobject<UBoxComponent>(FName("TriggerVolume"));
+	PressurePad = CreateDefaultSubobject<UStaticMeshComponent>(FName("PressurePad"));
+
+	SetRootComponent(TriggerVolume);
+	PressurePad->SetupAttachment(TriggerVolume);
+}
+
+// Called when the game starts or when spawned
+void APlatformTrigger::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TriggerVolume->OnComponentBeginOverlap.AddDynamic(this, &APlatformTrigger::OnTriggerBeginOverlap);
+	TriggerVolume->OnComponentEndOverlap.AddDynamic(this, &APlatformTrigger::OnTriggerEndOverlap);
+	// TriggerVolume->OnComponentEndOverlap.AddDynamic(this, FName("OnTriggerEndOverlap"));
+}
+
+// Called every frame
+void APlatformTrigger::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APlatformTrigger::OnTriggerBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	PressurePad->AddLocalOffset(FVector(0.f, 0.f, -10.f));
+}
+
+void APlatformTrigger::OnTriggerEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	PressurePad->AddLocalOffset(FVector(0.f, 0.f, 10.f));
+}
+
---
+++ Source/PlatformJumperGame/Public/PlatformTrigger.h
@@ -0,0 +1,36 @@
+// Copyright (C) 2019 Yernar Aldabergenov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PlatformTrigger.generated.h"
+
+UCLASS()
+class PLATFORMJUMPERGAME_API APlatformTrigger : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APlatformTrigger();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true), Category = Components)
+		class UBoxComponent* TriggerVolume;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true), Category = Components)
+		UStaticMeshComponent* PressurePad;
+
+	UFUNCTION()
+		void OnTriggerBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+		void OnTriggerEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+};
