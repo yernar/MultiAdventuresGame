d2ca20d0cd1b028a2cfe0f23b73500f44a8a6814
Yernar Aldabergenov <yernar.aa@gmail.com>
2019-10-26 20:26:57 +0600

    Adding Interfaces; | Binding implementations with Game Instance.

---
+++ Source/PlatformJumperGame/Private/MenuSystems/MenuInterface.cpp
@@ -0,0 +1,6 @@
+// Copyright (C) 2019 Yernar Aldabergenov. All Rights Reserved.
+
+
+#include "MenuInterface.h"
+
+// Add default functionality here for any IMenuInterface functions that are not pure virtual.
---
+++ Source/PlatformJumperGame/Public/MenuSystems/MenuInterface.h
@@ -0,0 +1,25 @@
+// Copyright (C) 2019 Yernar Aldabergenov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "MenuInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UMenuInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class PLATFORMJUMPERGAME_API IMenuInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
